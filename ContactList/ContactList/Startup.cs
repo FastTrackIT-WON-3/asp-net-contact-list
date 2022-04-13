45eb992dc03169f7e8b2c22c6d58bbeb453c8e4d
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-04-13 21:30:09 +0300

    add menu link

--- ContactList/ContactList/Startup.cs
+++ ContactList/ContactList/Startup.cs
@@ -58,7 +58,7 @@ namespace ContactList
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=ContactList}/{action=Index}/{id?}");
             });
         }
     }
